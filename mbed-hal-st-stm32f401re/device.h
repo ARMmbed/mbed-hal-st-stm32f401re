85fa9e6d6c6de0d222e0e49dd5d52f89d4e19428
Wolfgang Betz <wolfgang.betz@st.com>
2015-10-13 17:14:42 +0200

    Inline with module coming from yotta

--- mbed-hal-st-stm32f401re/device.h
+++ mbed-hal-st-stm32f401re/device.h
@@ -40,13 +40,15 @@
 #define DEVICE_ANALOGOUT        0 // Not present on this device
 
 #define DEVICE_SERIAL           1
+#define DEVICE_SERIAL_ASYNCH    0
 
 #define DEVICE_I2C              1
 #define DEVICE_I2CSLAVE         1
+#define DEVICE_I2C_ASYNCH       0
 
 #define DEVICE_SPI              1
-#define DEVICE_SPI_ASYNCH       1
 #define DEVICE_SPISLAVE         1
+#define DEVICE_SPI_ASYNCH       1
 
 #define DEVICE_RTC              1
 
