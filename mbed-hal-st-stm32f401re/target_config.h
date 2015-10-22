b52c3d19537b8e3d672c2eec3c1abb9d6d0daf34
Wolfgang Betz <wolfgang.betz@st.com>
2015-10-22 11:07:21 +0200

    'lp_ticker.c' moved from 'mbed-hal-st-stm32f4' to this module Implemented also a lp_timer tick of 0.5ms

--- mbed-hal-st-stm32f401re/target_config.h
+++ mbed-hal-st-stm32f401re/target_config.h
@@ -18,7 +18,7 @@
 
 // Minar platform configuration
 
-#define MINAR_PLATFORM_TIME_BASE  1282                   // ticks per second
+#define MINAR_PLATFORM_TIME_BASE  2000                   // ticks per second
 #define MINAR_PLATFORM_MINIMUM_SLEEP 2                   // in ticks
 
 #define MODULE_SIZE_SPI         4
